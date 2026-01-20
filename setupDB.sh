41f9c3373cdd8fb6d9e521720e823c0483fa530a
ShreyasVH <shreyas.deve.1@gmail.com>
2026-01-20 07:52:50 +0530

    direnv allow after update

--- setupDB.sh
+++ setupDB.sh
@@ -3,3 +3,4 @@ RESPONSE=$(curl -u "$ELASTIC_USERNAME:$ELASTIC_PASSWORD" -X POST "https://$ELAST
 export PATH=$HOME/programs/jq/$JQ_VERSION/bin:$PATH
 API_KEY=$(echo "$RESPONSE" | jq -r '.encoded')
 echo "export ELASTIC_API_KEY=\"$API_KEY\"" >> .envrc
+direnv allow
